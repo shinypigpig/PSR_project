0fe10f6ad8a495296114d04d6a7b0f02dba74b60
shinypigpig <a98918754@gmail.com>
2016-11-19 13:11:18 +0800

    6 commit

--- MyApplication/app/src/main/java/com/example/s2012107/psr2/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/s2012107/psr2/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.s2012107.psr2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView iv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,5 +14,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+//    }package com.AndroidOnClick;
+//
+//    import android.app.Activity;
+//    import android.os.Bundle;
+//    import android.view.View;
+//    import android.widget.Toast;
+//
+//    public class AndroidOnClickActivity extends Activity {
+//        /** Called when the activity is first created. */
+//        @Override
+//        public void onCreate(Bundle savedInstanceState) {
+//            super.onCreate(savedInstanceState);
+//            setContentView(R.layout.main);
+//        }
+//
+//        public void onClick(View view){
+//            Toast.makeText(AndroidOnClickActivity.this,
+//                    "Button Clicked", Toast.LENGTH_LONG).show();
+
     }
+
 }
